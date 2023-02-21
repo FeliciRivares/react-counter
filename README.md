a1a6cccb268c3943fc262f151edcb29e9f215485
Karina Ostrovska <karina.ostrovsika@gmail.com>
2023-02-21 21:59:41 +0200

    add state

---
+++ README.md
@@ -0,0 +1,11 @@
+Counter
+
+A simple counter project.
+To create i use:
+
+* SCSS
+* React
+* Hook Use State 
+
+
+<img src="/src/intro.png" alt="Image of the finished project" title="Image of the finished project">
--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './index.scss';
+import React from 'react';
 
 function App() {
+  const [count, setCount] = React.useState(10)
+
+  const countMinus = () =>{
+    setCount(count - 1)
+  }
+  const countPlus = () =>{
+    setCount(count + 1)
+  }
+
   return (
     <div className="App">
       <div>
-        <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h2>Count:</h2>
+        <h1>{count}</h1>
+        <button onClick={countMinus} className="minus">- Minus</button>
+        <button onClick={countPlus} className="plus">Plus +</button>
       </div>
     </div>
   );
